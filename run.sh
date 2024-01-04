b9ce63e9d2caf83db49b48728b77e632a9e04fc0
Manish Mahajan <manish@secantecinc.com>
2024-01-03 21:10:18 -0800

    update

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ make clean
 sleep 15
 ps -ef | grep iverilog | awk '{print $2}' | xargs -I str -t kill -9 str
 sleep 15
-ls -ltr  /tmp | grep $USER | awk '{print $9}' | xargs -I str -t /bin/rm -rf str
+ls -ltr  /tmp | grep $USER | awk '{print $9}' | xargs -I str -t /bin/rm -rf /tmp/str
 sleep 15
 make clean
 sleep 15
