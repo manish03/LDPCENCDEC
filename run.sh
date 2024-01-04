6b683b46b4179a505a3e6604824605ebc8a4f8cf
Manish Mahajan <manish@secantecinc.com>
2024-01-03 20:16:31 -0800

    update

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@ make clean
 sleep 15
 ps -ef | grep iverilog | awk '{print $2}' | xargs -I str -t kill -9 str
 sleep 15
+ls -ltr  /tmp | grep $USER | awk '{print $9}' | xargs -I str -t /bin/rm -rf str
+sleep 15
 make clean
 sleep 15
 make verify-wb_port-rtl
