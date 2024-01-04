871f04a4be8c27c64402a491e2ccfb7d38f3b8a8
Manish Mahajan <manish@secantecinc.com>
2024-01-03 20:57:05 -0800

    update

--- run.sh
+++ run.sh
@@ -7,3 +7,17 @@ sleep 15
 make clean
 sleep 15
 make verify-wb_port-rtl
+#verilog -Ttyp -DFUNCTIONAL -DSIM -DUSE_POWER_PINS -DUNIT_DELAY=#1 -g2012 \
+#        -f/media/psf/1TBsljt1/sljt1/sljt1.comp.xfer/backupcvs/hcb1/hcb/LDPCENCDEC/mgmt_core_wrapper/verilog/includes/includes.rtl.caravel \
+#        -f/media/psf/1TBsljt1/sljt1/sljt1.comp.xfer/backupcvs/hcb1/hcb/LDPCENCDEC/verilog/includes/includes.rtl.caravel_user_project -o wb_port.vvp wb_port_tb.v
+
+
+
+#setenv CARAVEL_ROOT /media/psf/1TBsljt1/sljt1/sljt1.comp.xfer/backupcvs/hcb1/hcb/LDPCENCDEC/caravel
+#setenv CARAVEL_PATH $CARAVEL_ROOT/../caravel/verilog
+#setenv VERILOG_PATH $CARAVEL_ROOT/../mgmt_core_wrapper/verilog
+#setenv USER_PROJECT_VERILOG $CARAVEL_ROOT/../verilog
+#
+#iverilog -Ttyp -DFUNCTIONAL -DSIM -DUSE_POWER_PINS -DUNIT_DELAY=#1 -g2012 -f$CARAVEL_ROOT/../mgmt_core_wrapper/verilog/includes/includes.rtl.caravel -f$CARAVEL_ROOT/../verilog/includes/includes.rtl.caravel_user_project -o wb_port.vvp wb_port_tb.v
+#
+
