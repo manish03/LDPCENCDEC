7df30a962dc91aa0d58157a4e5fa6c3949e6936c
Manish Mahajan <manish03@gmail.com>
2024-01-16 16:41:45 -0800

    update for working LDPC

--- verilog/dv/wb_port/wb_port_tb.v
+++ verilog/dv/wb_port/wb_port_tb.v
@@ -144,7 +144,7 @@ module wb_port_tb;
 
 		// Repeat cycles of 1000 clock edges as needed to complete testbench
 		repeat (70) begin
-			repeat (1000) @(posedge clock);
+			repeat (10000) @(posedge clock);
 			// $display("+1000 cycles");
 		end
 		$display("%c[1;31m",27);
